7c11252a16881297e27609e758a9b839e6078ad5
TthanhGithub <kaiapussale@gmail.com>
2025-11-03 23:57:14 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    const main = document.getElementById('main');
-    main.classList.remove('hidden');
-    requestAnimationFrame(() => {
-      main.style.opacity = '1';
-    });
-  }, 2500);
-});
